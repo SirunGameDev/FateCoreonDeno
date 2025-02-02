3139b3ab1a5d93d9a598a9f3ff25ca82a73a0702
SirunGameDev <sirun.game.dev@gmail.com>
2025-02-02 20:37:28 +0100

    First Meaningful commit, docker and deno added, vite react installed

---
+++ app/FateCore-Browser/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+// @deno-types="@types/react"
+import { useState } from 'react'
+// @ts-expect-error Unable to infer type at the moment
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Fate Core on Deno</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
